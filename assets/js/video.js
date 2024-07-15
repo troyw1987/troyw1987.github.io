fd0b99188ab0619e71e27a795b45a6d7a011193e
troyw1987 <troygregorywilson1@gmail.com>
2024-07-15 01:41:26 -0700

    Restructured the files, added an indicator for how many possibilities there are for songs in the song indicator and added Purple Kisses to the pool

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,24 +1,27 @@
 const videoMap = new Map();
 const scrolltextMap = new Map();
 
-videoMap.set(0,"assets/SSS.mp4") // Video 1
+videoMap.set(0,"assets/videos/SSS.mp4") // Video 1
 scrolltextMap.set(0,"Sexual Seduction - Snoop Dogg");
 
-videoMap.set(1,"assets/LSD.mp4") // Video 2
+videoMap.set(1,"assets/videos/LSD.mp4") // Video 2
 scrolltextMap.set(1,"L$D (Love $ex Dreams) - A$AP Rocky");
 
-videoMap.set(2,"assets/WSP.mp4") // Video 3
+videoMap.set(2,"assets/videos/WSP.mp4") // Video 3
 scrolltextMap.set(2,"Wassup - A$AP Rocky");
 
-videoMap.set(3,"assets/ADHD.mp4") // Video 4
+videoMap.set(3,"assets/videos/ADHD.mp4") // Video 4
 scrolltextMap.set(3,"A.D.H.D - Kendrick Lamar");
 
-videoMap.set(4,"assets/CAD.mp4") // Video 5
+videoMap.set(4,"assets/videos/CAD.mp4") // Video 5
 scrolltextMap.set(4,"Crunk Aint' Dead - Duke Deuce");
 
-var randomNumberZeroToOne = Math.floor(Math.random()*5);
+videoMap.set(5,"assets/videos/PUK.mp4")
+scrolltextMap.set(5,"Purple Kisses - A$AP Rocky")
+
+var randomNumberZeroToOne = Math.floor(Math.random()*6);
 var currentVideo = videoMap.get(randomNumberZeroToOne);
-var currentVideoScrollText = scrolltextMap.get(randomNumberZeroToOne);
+var currentVideoScrollText = scrolltextMap.get(randomNumberZeroToOne) + " (song " + (randomNumberZeroToOne+1) +"/6)";
 
 document.getElementById('welcome-screen').addEventListener('click', function() { // starts video
 
